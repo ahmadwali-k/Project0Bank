cc1b05b19a85cbacf72d595d66af7b292f0440b6
Cathy Nguyen <Smileycathy1128@gmail.com>
2021-11-04 21:20:57 -0400

    the welcome screen

--- bookstore/src/main/java/Options.java
+++ bookstore/src/main/java/Options.java
@@ -1,10 +1,42 @@
 public class Options {
     static void welcome() {
-        System.out.println(
-            "1 Log in\n"+
-            "2 Register\n"+
-            "3 Exit"
-        );
-        DaoFactory.getScanner().nextLine();
+        boolean loggedIn = false;
+        do {
+            System.out.println(
+                "1 Log in\n"+
+                "2 Register\n"+
+                "3 Exit"
+            );
+            
+            int num1;
+            try {
+                num1 = DaoFactory.getScanner().nextInt();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                System.out.println("Cannot accept strings");
+                continue;
+            }            
+            switch (num1) {
+                case 1: logIn();
+                    break;
+                case 2: register();
+                    break;
+                case 3: return; // TODO: change if needed
+                    // break;
+                default:
+                    break;
+            }
+
+        } while (!loggedIn);
+
     }
+    
+    static void logIn() {
+        
+    }
+    
+    static void register() {
+        
+    }
+    
 }
